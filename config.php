cad95fdd62d97a4e047b5cba432f566bef5e1466
Ben Trachtenberg <btr1975@gmail.com>
2017-07-24 09:09:36 -0500

    Working on it

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+
+## Check http://www.observium.org/docs/config_options/ for documentation of possible settings
+
+// Database config ---  This MUST be configured
+$config['db_extension'] = 'mysqli';
+$config['db_host']      = 'mysql-observium-db';
+$config['db_user']      = 'observeuser';
+$config['db_pass']      = '123456';
+$config['db_name']      = 'observium';
+
+// Base directory
+#$config['install_dir'] = "/opt/observium";
+
+// Default community list to use when adding/discovering
+$config['snmp']['community'] = array("public");
+
+// Authentication Model
+$config['auth_mechanism'] = "mysql";    // default, other options: ldap, http-auth, please see documentation for config help
+
+// Enable alerter
+// $config['poller-wrapper']['alerter'] = TRUE;
+
+//$config['web_show_disabled'] = FALSE;    // Show or not disabled devices on major pages.
+
+// Set up a default alerter (email to a single address)
+//$config['email']['default']        = "user@your-domain";
+//$config['email']['from']           = "Observium <observium@your-domain>";
+//$config['email']['default_only']   = TRUE;
+
+// End config.php
+
